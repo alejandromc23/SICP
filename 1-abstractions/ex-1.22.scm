70844455b3a6d5a725ac4e1e915a3790d54c3b98
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-05-25 17:43:45 +0200

    implement smallest primes starting from range

---
+++ 1-abstractions/ex-1.22.scm
@@ -0,0 +1,52 @@
+(define (smallest-divisor n)
+    (define (find-divisor n current)
+        (cond ((> (square current) n) n)
+              ((divides? current n) current)
+              (else (find-divisor n (+ current 1)))
+        )
+    )
+
+    (define (divides? a b)
+        (= (remainder b a) 0)
+    )
+
+    (find-divisor n 2)
+)
+
+(define (prime? n)
+    (= n (smallest-divisor n))
+)
+
+(define (timed-prime-test n)
+    (newline)
+    (display n)
+    (start-prime-test n (runtime))
+)
+
+(define (start-prime-test n start-time)
+    (if (prime? n)
+        (report-prime (- (runtime) start-time))
+    )
+)
+
+(define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time)
+)
+
+(define (search-for-primes current)
+    (define (search-for-primes-iter current found)
+        (cond ((= found 3) true)
+            ((even? current) (search-for-primes-iter (+ current 1) found))
+            ((prime? current) (timed-prime-test current) (search-for-primes-iter (+ current 2) (+ found 1)))
+            (else (search-for-primes-iter (+ current 2) found))
+        )
+    )
+
+    (search-for-primes-iter current 0)
+)
+
+(search-for-primes 1000)
+(search-for-primes 10000)
+(search-for-primes 100000)
+(search-for-primes 1000000)
