1c3bd0bc934254483c38418936d372cb24ee9352
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-05-30 06:55:55 +0200

    implement euler fraction

---
+++ 1-abstractions/ex-1.38.scm
@@ -0,0 +1,36 @@
+(define (cont-frac-recursive n d k)
+  (define (recursive-iter i)
+    (if (= i k)
+        (/ (n i) (d i))
+        (/ (n i) (+ (d i) (recursive-iter (+ i 1)))))
+  )
+
+  (recursive-iter 1)
+)
+
+(define (cont-frac-iterative n d k)
+  (define (iter i result)
+    (if (= i 0)
+        result
+        (iter (- i 1) (/ (n i) (+ (d i) result)))))
+  (iter k 0)
+)
+
+(define (epsilon) 0.0001)
+
+(define (cont-frac n d k)
+  (define (iter i result)
+    (if (< (abs (- result (cont-frac-iterative n d (+ i 1)))) (epsilon))
+        (begin (newline) (display "k = ") (display (+ i 1)) (newline) result)
+        (iter (+ i 1) (cont-frac-iterative n d (+ i 1)))))
+  
+  (iter 1 0)
+)
+
+(define (euler-fraction i)
+  (if (= (modulo i 3) 2)
+      (* 2(/ (+ i 1) 3))
+      1)
+)
+
+(cont-frac (lambda (i) 1.0) euler-fraction 1)
