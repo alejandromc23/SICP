a835ffcc37dae64cbf7b2c954c6991194361d455
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-06-06 12:15:31 +0200

    implemen map, length and append using sequence operations

---
+++ 2-data-abstractions/ex-2.33.scm
@@ -0,0 +1,30 @@
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) '() sequence))
+
+; Test
+(map (lambda (x) (* x x)) (list 1 2 3 4 5))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+; Test
+(append (list 1 2 3) (list 4 5 6))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+
+; Test
+(length (list 1 2 3 4 5))
