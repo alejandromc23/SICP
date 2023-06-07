ffb1291b37b26bfa09da699f5bf4dc1598db24c9
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-06-07 08:50:59 +0200

    implement ordered triple sums

---
+++ 2-data-abstractions/ex-2.41.scm
@@ -0,0 +1,42 @@
+(define nil '())
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (unique-triples n)
+  (flatmap (lambda (i)
+             (flatmap (lambda (j)
+                        (map (lambda (k) (list i j k))
+                             (enumerate-interval 1 (- j 1))))
+                     (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+; Test
+(unique-triples 5)
+
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (ordered-triples-sum n)
+  (filter (lambda (triple)
+            (= (+ (car triple) (cadr triple) (caddr triple)) n))
+          (unique-triples n)))
+
+; Test
+(ordered-triples-sum 12)
