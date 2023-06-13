7693622dfaca18aa52b4f25b62bcbcfe2e40027a
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-06-13 06:22:23 +0200

    implement direct programming deriv sum and product

---
+++ 2-data-abstractions/ex-2.73.scm
@@ -0,0 +1,36 @@
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) 0)
+         (else ((get 'deriv (operator exp)) (operands exp) var))))
+
+(define (install-sum-deriv-operations)
+  (define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+          ((=number? a2 0) a1)
+          ((and (number? a1) (number? a2)) (+ a1 a2))
+          (else (list '+ a1 a2))))
+
+  (define (sum operands var)
+    (make-sum (deriv (car operands) var)
+              (deriv (cadr operands) var)))
+
+  (put 'deriv '+ sum)
+  'done)
+
+(define (install-product-deriv-operations)
+  (define (make-product m1 m2)
+    (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+          ((=number? m1 1) m2)
+          ((=number? m2 1) m1)
+          ((and (number? m1) (number? m2)) (* m1 m2))
+          (else (list '* m1 m2))))
+
+  (define (product operands var)
+    (make-sum
+     (make-product (car operands)
+                   (deriv (cadr operands) var))
+     (make-product (deriv (car operands) var)
+                   (cadr operands))))
+
+  (put 'deriv '* product)
+  'done)
