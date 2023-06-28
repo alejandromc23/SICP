e869043ab98d32d59a9211d9316d9f8121a06f4c
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-06-28 12:45:07 +0200

    implement deque

--- 3-modularity-objects-state/ex-3.23.scm
+++ 3-modularity-objects-state/ex-3.23.scm
@@ -17,7 +17,10 @@
   (set-cdr! node next))
 
 (define (set-previous! node previous)
-  (set-cdr! (car node) (node-pair previous)))
+  (set-cdr! (car node) previous))
+
+(define (set-value! node value)
+  (set-car! (car node) value))
 
 ; Test node
 (define n1 (make-node 1 '() '()))
@@ -30,12 +33,10 @@
 (node-value n1) ; 1
 (node-value n2) ; 2
 (node-value n3) ; 3
-(node-next n1) ; n2
-(node-next n2) ; n3
-(node-next n3) ; ()
-(node-previous n1) ; ()
-(node-previous n2) ; n1
-(node-previous n3) ; n2 
+(node-value (node-next n1)) ; 2
+(node-value (node-next n2)) ; 3
+(node-value (node-previous n2)) ; 1
+(node-value (node-previous n3)) ; 2
 
 (define (make-deque)
   (cons '() '()))
@@ -49,48 +50,50 @@
 (define (empty-deque? deque)
   (null? (front-deque deque)))
 
+(define (print-deque deque)
+  (define (helper node acc)
+    (if (null? node)
+        acc
+        (helper (node-next node) (cons (node-value node) acc))))
+  (helper (front-deque deque) '()))
+
 (define (front-insert-deque! deque value)
-  (let ((new-node (make-node value '() '())))
+  (let ((new-node (make-node value '() (front-deque deque))))
     (if (empty-deque? deque)
         (begin
           (set-car! deque new-node)
           (set-cdr! deque new-node))
         (begin
-          (set-next! new-node (front-deque deque))
           (set-previous! (front-deque deque) new-node)
           (set-car! deque new-node)))))
 
 ; Test front-insert-deque!
 (define d1 (make-deque))
 (front-insert-deque! d1 1)
-d1
+(print-deque d1)
 (front-insert-deque! d1 2)
-d1
+(print-deque d1)
 (front-insert-deque! d1 3)
-d1
+(print-deque d1)
 
 (define (rear-insert-deque! deque value)
-  (let ((new-node (make-node value '() '()))
-        (old-rear (rear-deque deque)))
-    (newline)
-    (display new-node)
+  (let ((new-node (make-node value (rear-deque deque) '())))
     (if (empty-deque? deque)
         (begin
           (set-car! deque new-node)
           (set-cdr! deque new-node))
         (begin
-          (set-previous! new-node old-rear)
           (set-next! (rear-deque deque) new-node)
           (set-cdr! deque new-node)))))
 
 ; Test rear-insert-deque!
 (define d2 (make-deque))
 (rear-insert-deque! d2 1)
-d2
+(print-deque d2)
 (rear-insert-deque! d2 2)
-d2
+(print-deque d2)
 (rear-insert-deque! d2 3)
-d2
+(print-deque d2)
 
 (define (front-delete-deque! deque)
   (if (empty-deque? deque)
@@ -109,10 +112,54 @@ d2
 (front-insert-deque! d3 1)
 (front-insert-deque! d3 2)
 (front-insert-deque! d3 3)
-d3
+(print-deque d3)
 (front-delete-deque! d3)
-d3
+(print-deque d3)
 (front-delete-deque! d3)
-d3
+(print-deque d3)
 (front-delete-deque! d3)
-d3
+(print-deque d3)
+
+(define (rear-delete-deque! deque)
+  (if (empty-deque? deque)
+      (error "Empty deque -- REAR-DELETE-DEQUE!")
+      (begin
+        (if (eq? (front-deque deque) (rear-deque deque))
+            (begin
+              (set-car! deque '())
+              (set-cdr! deque '()))
+            (begin
+              (set-cdr! deque (node-previous (rear-deque deque)))
+              (set-next! (rear-deque deque) '()))))))
+
+; Test rear-delete-deque!
+(define d4 (make-deque))
+(rear-insert-deque! d4 1)
+(rear-insert-deque! d4 2)
+(rear-insert-deque! d4 3)
+(print-deque d4)
+(rear-delete-deque! d4)
+(print-deque d4)
+(rear-delete-deque! d4)
+(print-deque d4)
+(rear-delete-deque! d4)
+(print-deque d4)
+
+; Test all together
+(define d5 (make-deque))
+(front-insert-deque! d5 1)
+(print-deque d5) ; (1)
+(front-insert-deque! d5 2)
+(print-deque d5) ; (1 2)
+(rear-delete-deque! d5)
+(print-deque d5) ; (2)
+(front-insert-deque! d5 3)
+(print-deque d5) ; (2 3)
+(rear-insert-deque! d5 4)
+(print-deque d5) ; (4 2 3)
+(rear-delete-deque! d5)
+(print-deque d5) ; (2 3)
+(rear-delete-deque! d5)
+(print-deque d5) ; (3)
+(front-delete-deque! d5)
+(print-deque d5) ; ()
