78b78356f2104b0d69a5fc0da97245bd21d3614e
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-07-30 10:41:12 +0200

    implement serialized exchange

---
+++ 3-modularity-objects-state/ex-3.43.scm
@@ -0,0 +1,12 @@
+(define (exchange account1 account2)
+  (let ((difference (- (balance account1)
+                       (balance account2))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
+
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    ((serializer1 (serializer2 exchange))
+     account1
+     account2)))
