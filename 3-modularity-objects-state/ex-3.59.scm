13bd4ad7502d67acada853dd3187ad60ab2e6fe4
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-09-21 08:24:24 +0200

    sine and cosine series

--- 3-modularity-objects-state/ex-3.59.scm
+++ 3-modularity-objects-state/ex-3.59.scm
@@ -5,6 +5,10 @@
 (define (add-streams s1 s2)
   (stream-map + s1 s2))
 
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
 ; Part a
 (define (mul-streams s1 s2)
   (stream-map * s1 s2))
@@ -34,3 +38,22 @@
 (stream-ref exp-series 0)
 (stream-ref exp-series 1)
 (stream-ref exp-series 2)
+(stream-ref exp-series 3)
+(stream-ref exp-series 4)
+(stream-ref exp-series 5)
+
+(define cosine-series (cons-stream 1 (integrate-series (scale-stream sine-series -1))))
+(define sine-series (cons-stream 0 (integrate-series cosine-series)))
+
+; Test
+(stream-ref cosine-series 0)
+(stream-ref cosine-series 1)
+(stream-ref cosine-series 2)
+(stream-ref cosine-series 3)
+(stream-ref cosine-series 4)
+
+(stream-ref sine-series 0)
+(stream-ref sine-series 1)
+(stream-ref sine-series 2)
+(stream-ref sine-series 3)
+(stream-ref sine-series 4)
